e9b6d227646236d9c0541f9dc5b5acf384769767
unknown <you@example.com>
2017-04-27 22:13:36 +0300

    index search

--- app/include/az-search.php
+++ app/include/az-search.php
@@ -1,7 +1,29 @@
-section.az-search
+<section class="az-search">
+	<div class="container">
+		<form action="/">
+			<div class="az-search-wrap clearfix">
+				<div class="az-search1">
+					<select class="ah-catselect">
+						<option>Все регионы</option>
+						<option>регион2</option>
+						<option>регион3</option>
+					</select>
+				</div>
+				<div class="az-search2">
+					<input type="text" placeholder="Что будем искать? Например, платье?">
+				</div>
+				<div class="az-search3">
+					<select class="ah-catselect">
+						<option>Все разделы</option>
+						<option>раздел2</option>
+						<option>раздел3</option>
+					</select>
+				</div>
+				<div class="az-search4">
+					<input type="submit" value="Искать">
+				</div>
+			</div>
+		</form>
+	</div>
+</section>
 
-<select class="ah-catselect">
-	  <option>по 12 на страницу</option>
-	  <option>по 12 на страницу2</option>
-	  <option>по 12 на страницу3</option>
-	</select>
