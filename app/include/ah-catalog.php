1c1f61db38684d627a7d7c82c3fef1642b075ad2
iwebnal <iwebnal@gmail.com>
2017-04-27 11:20:42 +0300

    catalog

---
+++ app/include/ah-catalog.php
@@ -0,0 +1,22 @@
+<div class="row">
+	<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+		<div class="ah-catalog-selectbox">
+			<div class="ah-rowlinks">
+				<a href="#"></a>
+				<a href="#"></a>
+			</div>
+			<div class="ah-selectbox">
+				<select class="ah-catselect1">
+				  <option>по 12 на страницу</option>
+				  <option>по 12 на страницу</option>
+				  <option>по 12 на страницу</option>
+				</select>
+				<select class="ah-catselect2">
+				  <option>от дешевых к дорогим</option>
+				  <option>от дешевых к дорогим</option>
+				  <option>от дешевых к дорогим</option>
+				</select>
+			</div>
+		</div>
+	</div>
+</div>
--- app/js/common.js
+++ app/js/common.js
@@ -1,3 +1,21 @@
 $(function() {
 
+	if($('.ah-catselect1').length>0){
+
+		$('.ah-catselect1').selectpicker({
+		  size: 4
+		});
+
+	}
+
+	if($('.ah-catselect2').length>0){
+
+		$('.ah-catselect2').selectpicker({
+		  size: 4
+		});
+		
+	}
+
+
+
 });
