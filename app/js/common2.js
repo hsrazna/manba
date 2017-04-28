f2e98f0ea18928bfac91873a6e3cc2bfab7157cd
hsrazna <anzar.terek@gmail.com>
2017-04-28 20:36:56 +0300

    index tablet

--- app/js/common2.js
+++ app/js/common2.js
@@ -1,9 +1,10 @@
 $(function() {
+
 	$(".toggle-mnu").click(function() {
-      $(this).toggleClass("on");
-      $('.az-menu-list').toggleClass("on");
-      return false;
-  });
+		$(this).toggleClass("on");
+		$('.az-menu-list').toggleClass("on");
+		return false;
+	});
 
 	$('.az-carousel').owlCarousel({
 		items: 1,
@@ -13,5 +14,21 @@ $(function() {
 
 	$( "#tabs" ).tabs({ active: 0 });
 
-	
+	$(window).click(function(){
+		$(".az-cat>li").removeClass("active");
+	});
+
+	$(".az-cat>li").click(function(event){
+		event.stopPropagation();
+		if($(this).hasClass("active")){
+			$(this).removeClass("active");
+		} else {
+			$(".az-cat>li").removeClass("active");
+			$(this).addClass("active");
+		}
+		return false;
+	});
+
+
+
 });
--- app/js/scripts.js
+++ app/js/scripts.js
@@ -13,11 +13,12 @@ $(function() {
 });
 
 $(function() {
+
 	$(".toggle-mnu").click(function() {
-      $(this).toggleClass("on");
-      $('.az-menu-list').toggleClass("on");
-      return false;
-  });
+		$(this).toggleClass("on");
+		$('.az-menu-list').toggleClass("on");
+		return false;
+	});
 
 	$('.az-carousel').owlCarousel({
 		items: 1,
@@ -27,5 +28,21 @@ $(function() {
 
 	$( "#tabs" ).tabs({ active: 0 });
 
-	
+	$(window).click(function(){
+		$(".az-cat>li").removeClass("active");
+	});
+
+	$(".az-cat>li").click(function(event){
+		event.stopPropagation();
+		if($(this).hasClass("active")){
+			$(this).removeClass("active");
+		} else {
+			$(".az-cat>li").removeClass("active");
+			$(this).addClass("active");
+		}
+		return false;
+	});
+
+
+
 });
