d42e120e267e621aa3d62b55f151fbb51ee3bc33
iwebnal <iwebnal@gmail.com>
2017-04-28 18:00:29 +0300

    a

---
+++ app/include/ah-person.php
@@ -0,0 +1,11 @@
+<div class="row">
+	<div class="col-lg-4 col-md-4 col-sm-4 col-xs-12">
+		<div class="ah-person-form">
+			<span>Мои данные</span>
+			<div class="ah-person-imgbox">
+				<img src="img/ah-person-men.png" alt="person men">
+			</div>
+			<span>В первую очередь я</span>
+		</div>
+	</div>
+</div>
---
+++ app/include/ah-sidebar.php
@@ -0,0 +1,9 @@
+<section class="ah-sidebar">
+    <div class="container">
+    	<div class="row">
+			<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+				
+			</div>
+		</div>
+	</div>
+</section>
