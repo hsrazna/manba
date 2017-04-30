0e9ecef52013d694f784dc8ebd3c55a713bacaf2
hsrazna <anzar.terek@gmail.com>
2017-04-30 13:01:13 +0300

    popup

--- app/js/scripts.js
+++ app/js/scripts.js
@@ -54,6 +54,20 @@ $(function() {
 		return false;
 	});
 
+	$('.popup-with-move-anim').magnificPopup({
+		type: 'inline',
 
+		fixedContentPos: false,
+		fixedBgPos: true,
+
+		overflowY: 'auto',
+
+		closeBtnInside: true,
+		preloader: false,
+		
+		midClick: true,
+		removalDelay: 300,
+		mainClass: 'my-mfp-slide-bottom'
+	});
 
 });
