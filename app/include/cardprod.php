8414874ce9c831c36b5935adfbb113c3005bf755
iwebnal <iwebnal@gmail.com>
2017-05-10 17:19:02 +0300

    a

--- app/include/cardprod.php
+++ app/include/cardprod.php
@@ -3,7 +3,7 @@
 		<div class="row">
 			<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
 				<div class="row">
-					<div class="col-lg-4 col-md-4 col-sm-12 col-xs-12">
+					<div class="col-lg-4 col-md-4 col-sm-5 col-xs-12">
 						<div class="ah-cardprod-slidebox">
 							<div class="ah-cardprod-slider">
 								<div class="item" data-hash="zero">
@@ -43,19 +43,19 @@
 							</div>
 						</div>
 					</div>
-					<div class="col-lg-8 col-md-8 col-sm-12 col-xs-12">
+					<div class="col-lg-8 col-md-8 col-sm-7 col-xs-12">
 						<div class="row">
 							<div class="col-lg-8 col-md-8 col-sm-12 col-xs-12">
-								<span>Пончо весеннее черное с вставками красной и черной с рисунком, длинное, утепленное</span>
-								<div class="ah-catitem-starsrow">
-									<ul class="ah-catitem-starsbox">
+								<span class="ah-cardprod-title">Пончо весеннее черное с вставками красной и черной с рисунком, длинное, утепленное</span>
+								<div class="ah-catitem-starsrow ah-cardprod-starsrow1">
+									<ul class="ah-catitem-starsbox ah-cardprod-starsbox">
 										<li><span class="ah-str1"></span></li>
 										<li><span class="ah-str1"></span></li>
 										<li><span class="ah-str1"></span></li>
 										<li><span class="ah-str1"></span></li>
 										<li><span class="ah-str0"></span></li>
 									</ul>
-									<ul class="ah-catitem-arrsbox">
+									<ul class="ah-catitem-arrsbox ah-cardprod-arrsbox">
 										<li><span class="ah-arrtop">777</span></li>
 										<li><span class="ah-arrbot">5</span></li>
 									</ul>
@@ -69,10 +69,83 @@
 								</ul>
 							</div>
 							<div class="col-lg-4 col-md-4 col-sm-12 col-xs-12">
-
+								<div class="ah-cardprod-soclink">
+									<span class="ah-cardprod-rigspan">Продавец</span>
+									<span class="ah-cardprod-rigspan1">магазин «Aurora International»</span>
+									<div class="ah-catitem-starsrow">
+										<ul class="ah-catitem-starsbox ah-cardprod-starsbox">
+											<li><span class="ah-str1"></span></li>
+											<li><span class="ah-str1"></span></li>
+											<li><span class="ah-str1"></span></li>
+											<li><span class="ah-str1"></span></li>
+											<li><span class="ah-str0"></span></li>
+										</ul>
+										<ul class="ah-catitem-arrsbox">
+											<li><span class="ah-arrtop">879</span></li>
+											<li><span class="ah-arrbot">5</span></li>
+										</ul>
+									</div>
+									<a href="#" class="ah-cardprod-riglink">Задать вопрос</a>
+									<div class="ah-cardprod-socbox">
+										<a href="#" class="ah-cardprod-soc1"></a>
+										<a href="#" class="ah-cardprod-soc2"></a>
+									</div>
+								</div>
+							</div>
+							<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+								<div class="ah-cardprod-selectbox">
+									<div class="ah-cardprod-select ah-cardprod-marg">
+										<select class="ah-catselect">
+										  <option>Черное с красным</option>
+										  <option>Черное с красным1</option>
+										  <option>Черное с красным2</option>
+										</select>
+									</div>
+									<div class="ah-cardprod-select">
+										<select class="ah-catselect">
+										  <option>M</option>
+										  <option>M1</option>
+										  <option>M2</option>
+										</select>
+									</div>
+								</div>
+								<div class="ah-cardprod-selectbox">
+									<div class="ah-cardprod-select ah-cardprod-marg">
+										<select class="ah-catselect">
+										  <option>С длинным рукавом</option>
+										  <option>С длинным рукавом1</option>
+										  <option>С длинным рукавом2</option>
+										</select>
+									</div>
+									<div class="ah-cardprod-select">
+										<select class="ah-catselect">
+										  <option>Без подкладки</option>
+										  <option>Без подкладки1</option>
+										  <option>Без подкладки2</option>
+										</select>
+									</div>
+								</div>
+							</div>
+							<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+								<div class="ah-tjswrapp">
+									<div class="ah-tjsbox">
+										<span class="ah-tjsbox-span">6,00 TJS</span>
+										<span class="ah-tjsbox-span ah-tjsbox-span1">или</span>
+										<span class="ah-tjsbox-span">4,00 TJS + 3 бонуса</span>
+									</div>
+									<div class="ah-tjslinks">
+										<a href="#" class="ah-tjslink ah-tjslink-marg">Купить</a>
+										<a href="#" class="ah-tjslink ah-tjslink1">Торговаться</a>
+										<a href="#" class="ah-tjslinks-span">Кредит или рассрочка</a>
+									</div>
+								</div>
 							</div>
 						</div>
 					</div>
+					<div class="clearfix"></div>
+					<!-- <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+						
+					</div> -->
 				</div>
 			</div>
 		</div>
