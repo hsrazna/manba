c6f22be0b809f1a790a402c5d863d2d771620e56
hsrazna <anzar.terek@gmail.com>
2017-05-11 18:55:44 +0300

    person product start

---
+++ app/include/az-personal-product.php
@@ -0,0 +1,20 @@
+<div class="az-personal-product">
+	<h2>Выставленные мной товары</h2>
+	<ul class="az-pproducts">
+		<li class="az-pp-item">
+			<div class="az-pp-top clearfix">
+				<div class="az-pp-top-left">
+					<a href="#" class="az-moderation">Модерация</a>
+				</div>
+				<div class="az-pp-top-right">
+					<a href="#" class="az-pp-pay">Cделать платным</a>
+					<a href="#" class="az-pp-correct">редактировать</a>
+					<a href="#" class="az-pp-slide-link">развернуть</a>
+				</div>
+			</div>
+			<div class="az-pp-middle"></div>
+			<div class="az-pp-bottom"></div>
+			<div class="az-pp-slide"></div>
+		</li>
+	</ul>
+</div>
